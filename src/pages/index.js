14263c57a4039209a82740dcce6e1e47dffb479f
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-01 15:35:45 +0530

    initial commit :fire:

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Text } from "@chakra-ui/react"
+
+function IndexPage() {
+  return (
+    <Box p={8}>
+      <Text fontSize="xl">hkbShopify website is coming sooon...</Text>
+    </Box>
+  )
+}
+
+export default IndexPage
