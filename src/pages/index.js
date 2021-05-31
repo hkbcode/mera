b5b2214db8f7942f4aea6d699c87cf50605c2298
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-05-31 07:39:03 +0530

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, Button } from "@chakra-ui/react"
 
 function IndexPage() {
   return (
     <Box p={8}>
       <Text fontSize="xl">Best Gatsbyjs Site with ChakraUI</Text>
+      <Button colorScheme="blue">Start Coding</Button>
     </Box>
   )
 }
